21bfcf0145f428f7fe9902cfc50599e7d7951af7
Jean-Yves LEBLEU <jylebleu@avencall.com>
2011-12-12 12:03:28 -0500

    renamed xivo_daemon, setup.py updated

--- xivo-ctid/setup.py
+++ xivo-ctid/setup.py
@@ -2,20 +2,19 @@
 
 from distutils.core import setup
 
-setup(name='xivo_ctid',
-      version='1.2',
-      description='XiVO CTI Server Daemon',
-      author='Avencall',
-      author_email='technique@proformatique.com',
-      url='http://xivo.fr/',
-      packages=['xivo_cti',
-                'xivo_cti.model',
-                'xivo_cti.alarm',
-                'xivo_cti.lists',
-                'xivo_cti.statistics',
-                'xivo_cti.dao',
-                'xivo_cti.dao.alchemy',],
-      data_files=[
-          ('/usr/sbin', ['xivo_ctid']),
-         ],
-     )
+setup(
+    name='xivo-ctid',
+    version='1.2',
+    description='XiVO CTI Server Daemon',
+    author='Avencall',
+    author_email='xivo-dev@lists.proformatique.com',
+    url='http://wiki.xivo.fr/',
+    packages=['xivo_cti',
+              'xivo_cti.model',
+              'xivo_cti.alarm',
+              'xivo_cti.lists',
+              'xivo_cti.statistics',
+              'xivo_cti.dao',
+              'xivo_cti.dao.alchemy',],
+    scripts=['bin/xivo-ctid'],
+)
