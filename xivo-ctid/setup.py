1530eb579e841749520edb8cd863d1049cba6400
Jean-Yves LEBLEU <jylebleu@avencall.com>
2011-12-12 11:38:02 -0500

    xivo_ctiservers renamed in xivo_cti

--- xivo-ctid/setup.py
+++ xivo-ctid/setup.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
 
-__version__ = "$Revision$ $Date$"
-
 from distutils.core import setup
 
-setup(name='xivo_daemon',
-      version='1.0',
+setup(name='xivo_ctid',
+      version='1.2',
       description='XiVO CTI Server Daemon',
-      author='Proformatique',
+      author='Avencall',
       author_email='technique@proformatique.com',
       url='http://xivo.fr/',
-      packages=['xivo_ctiservers',
-                'xivo_ctiservers.model',
-                'xivo_ctiservers.alarm',
-                'xivo_ctiservers.lists',
-                'xivo_ctiservers.statistics',
-                'xivo_ctiservers.dao',
-                'xivo_ctiservers.dao.alchemy',],
+      packages=['xivo_cti',
+                'xivo_cti.model',
+                'xivo_cti.alarm',
+                'xivo_cti.lists',
+                'xivo_cti.statistics',
+                'xivo_cti.dao',
+                'xivo_cti.dao.alchemy',],
       data_files=[
-          ('/usr/sbin', ['xivo_daemon']),
+          ('/usr/sbin', ['xivo_ctid']),
          ],
      )
