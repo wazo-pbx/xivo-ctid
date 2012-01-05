dadfe28c29c93bcc0de36854fbf45cda86f9d651
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-01-05 13:47:59 -0500

    [evol] Add a command queue to the CTICommandHandler

--- xivo-ctid/xivo_cti/cti/cti_command_handler.py
+++ xivo-ctid/xivo_cti/cti/cti_command_handler.py
@@ -1,4 +1,15 @@
+import Queue
+
+from xivo_cti.cti.cti_command_factory import CTICommandFactory
+
+
 class CTICommandHandler(object):
 
     def __init__(self):
-        pass
+        self._command_factory = CTICommandFactory()
+        self._commands_to_run = Queue.Queue()
+
+    def parse_message(self, message):
+        command_classes = self._command_factory.get_command(message)
+        for command_class in command_classes:
+            self._commands_to_run.put(command_class(message))
--- xivo-ctid/xivo_cti/cti/tests/test_cti_command_handler.py
+++ xivo-ctid/xivo_cti/cti/tests/test_cti_command_handler.py
@@ -1,12 +1,13 @@
 import unittest
 
 from xivo_cti.cti.cti_command_handler import CTICommandHandler
+from xivo_cti.cti.commands.invite_confroom import InviteConfroom
 
 
 class Test(unittest.TestCase):
 
     def setUp(self):
-        self._msg_1 = {"class": "invite-confroom",
+        self._msg_1 = {"class": "invite_confroom",
                        "commandid": 737000717,
                        "invitee": "user:pcmdev/3"}
 
@@ -17,6 +18,18 @@ class Test(unittest.TestCase):
         cti_handler = CTICommandHandler()
 
         self.assertTrue(cti_handler is not None)
+        self.assertTrue(cti_handler._command_factory is not None)
+
+    def test_parse_message(self):
+        cti_handler = CTICommandHandler()
+
+        self.assertTrue(cti_handler._commands_to_run.empty())
+
+        cti_handler.parse_message(self._msg_1)
+
+        self.assertEqual(cti_handler._commands_to_run.qsize(), 1)
+        command = cti_handler._commands_to_run.get(False)
+        self.assertTrue(isinstance(command, InviteConfroom))
 
 
 if __name__ == "__main__":
