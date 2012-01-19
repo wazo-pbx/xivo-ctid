784d203ba846f4b276b8aa9e460a50636edf710e
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-01-19 14:37:47 -0500

    [refactor] Remove redundant usage of __class__

--- xivo-ctid/xivo_cti/cti/cti_command_factory.py
+++ xivo-ctid/xivo_cti/cti/cti_command_factory.py
@@ -27,7 +27,7 @@ class CTICommandFactory(object):
     _registered_classes = set()
 
     def get_command(self, msg):
-        return [klass for klass in self.__class__._registered_classes if klass.match_message(msg)]
+        return [klass for klass in self._registered_classes if klass.match_message(msg)]
 
     @classmethod
     def register_class(cls, klass_to_register):
