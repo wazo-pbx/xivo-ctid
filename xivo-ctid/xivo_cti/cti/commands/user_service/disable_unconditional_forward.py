c27e0ce2b4505abef52cd3b2b07bba20471839dd
Jean-Yves LEBLEU <jylebleu@avencall.com>
2012-02-06 13:41:57 -0500

    use setforward for enable and destination
    #2997

--- xivo-ctid/xivo_cti/cti/commands/user_service/disable_unconditional_forward.py
+++ xivo-ctid/xivo_cti/cti/commands/user_service/disable_unconditional_forward.py
@@ -1,26 +1,20 @@
 from xivo_cti.cti.cti_command import CTICommand
 from xivo_cti.cti.cti_command_factory import CTICommandFactory
 from xivo_cti.cti.commands.set_user_service import SetUserService
+from xivo_cti.cti.commands.user_service.set_forward import SetForward
 
 
-class DisableUnconditionalForward(SetUserService):
+class DisableUnconditionalForward(SetForward):
 
     FUNCTION_NAME = 'fwd'
     ENABLE_NAME = 'enableunc'
-    DESTINATION = 'destunc'
+    DESTINATION_NAME = 'destunc'
 
     required_fields = [CTICommand.CLASS, SetUserService.FUNCTION, SetUserService.VALUE]
     conditions = [(CTICommand.CLASS, SetUserService.COMMAND_CLASS),
                   (SetUserService.FUNCTION, FUNCTION_NAME),
-                  ((SetUserService.VALUE,ENABLE_NAME), False)]
-    _callbacks = []
+                  ((SetUserService.VALUE, ENABLE_NAME), False)]
     _callbacks_with_params = []
-    
-    
-    def _init_from_dict(self, msg):
-        super(DisableUnconditionalForward, self)._init_from_dict(msg)
-        self.destination = msg[SetUserService.VALUE][self.DESTINATION]
-        self.enable = msg[SetUserService.VALUE][self.ENABLE_NAME]
 
 
 CTICommandFactory.register_class(DisableUnconditionalForward)
--- xivo-ctid/xivo_cti/cti/commands/user_service/enable_unconditional_forward.py
+++ xivo-ctid/xivo_cti/cti/commands/user_service/enable_unconditional_forward.py
@@ -1,26 +1,20 @@
 from xivo_cti.cti.cti_command import CTICommand
 from xivo_cti.cti.cti_command_factory import CTICommandFactory
 from xivo_cti.cti.commands.set_user_service import SetUserService
+from xivo_cti.cti.commands.user_service.set_forward import SetForward
 
 
-class EnableUnconditionalForward(SetUserService):
+class EnableUnconditionalForward(SetForward):
 
     FUNCTION_NAME = 'fwd'
     ENABLE_NAME = 'enableunc'
-    DESTINATION = 'destunc'
+    DESTINATION_NAME = 'destunc'
 
     required_fields = [CTICommand.CLASS, SetUserService.FUNCTION, SetUserService.VALUE]
     conditions = [(CTICommand.CLASS, SetUserService.COMMAND_CLASS),
                   (SetUserService.FUNCTION, FUNCTION_NAME),
-                  ((SetUserService.VALUE,ENABLE_NAME), True)]
-    _callbacks = []
+                  ((SetUserService.VALUE, ENABLE_NAME), True)]
     _callbacks_with_params = []
-    
-    
-    def _init_from_dict(self, msg):
-        super(EnableUnconditionalForward, self)._init_from_dict(msg)
-        self.destination = msg[SetUserService.VALUE][self.DESTINATION]
-        self.enable = msg[SetUserService.VALUE][self.ENABLE_NAME]
 
 
 CTICommandFactory.register_class(EnableUnconditionalForward)
