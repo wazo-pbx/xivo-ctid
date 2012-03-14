3f659f560fb7de18f6e9acb2b2bd8f91774acbc0
Sebastien Duthil <sduthil@avencall.com>
2012-03-14 18:30:13 -0400

    [evol] Starting implementing queuemember config awareness

---
+++ xivo-ctid/xivo_cti/services/tests/test_queuemember_service_manager.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8 :
+
+# Copyright (C) 2007-2011  Avencall
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Alternatively, XiVO CTI Server is available under other licenses directly
+# contracted with Pro-formatique SARL. See the LICENSE file at top of the
+# source tree or delivered in the installable package in which XiVO CTI Server
+# is distributed for more details.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from tests.mock import Mock, call
+from xivo_cti.services.queuemember_service_manager import QueueMemberServiceManager
+
+
+class TestUserServiceManager(unittest.TestCase):
+
+    def setUp(self):
+        self.queuemember_service_manager = QueueMemberServiceManager()
+
+    def test_update_config(self):
+        self.queuemember_service_manager.queuemember_dao = Mock()
+        self.queuemember_service_manager.queuemember_notifier = Mock()
+
+        self.queuemember_service_manager.update_config()
+
+        dao_method_calls = self.queuemember_service_manager.queuemember_dao.method_calls
+        self.assertTrue(dao_method_calls == [call.get_queuemembers()])
+        notifier_method_calls = self.queuemember_service_manager.queuemember_notifier.method_calls
+        self.assertTrue(notifier_method_calls == [call.config_updated()])
