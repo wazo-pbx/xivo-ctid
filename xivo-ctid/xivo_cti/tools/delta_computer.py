6092c6c1cba3c16eddea0b6b4eae2fa49265946b
Sebastien Duthil <sduthil@avencall.com>
2012-03-16 15:22:33 -0400

    [evol] Config delta computer module

---
+++ xivo-ctid/xivo_cti/tools/delta_computer.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# XiVO CTI Server
+# Copyright (C) 2009-2012  Avencall
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Alternatively, XiVO CTI Server is available under other licenses directly
+# contracted with Pro-formatique SARL. See the LICENSE file at top of the
+# source tree or delivered in the installable package in which XiVO CTI Server
+# is distributed for more details.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from collections import namedtuple
+
+DictDelta = namedtuple('DictDelta', ['add', 'change', 'delete'])
+
+
+class DeltaComputer(object):
+
+    @staticmethod
+    def compute_delta(new_dict, old_dict):
+        added_keys = list(set(new_dict) - set(old_dict))
+        added_items = dict([(item_key, new_dict[item_key]) for item_key in added_keys])
+        removed = list(set(old_dict) - set(new_dict))
+        changed = dict([(new_key, new_value) for (new_key, new_value) in new_dict.iteritems()
+                        if new_key in old_dict
+                        and old_dict[new_key] != new_dict[new_key]])
+        return DictDelta(added_items, changed, removed)
---
+++ xivo-ctid/xivo_cti/tools/tests/test_delta_computer.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+# XiVO CTI Server
+# Copyright (C) 2009-2012  Avencall
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Alternatively, XiVO CTI Server is available under other licenses directly
+# contracted with Pro-formatique SARL. See the LICENSE file at top of the
+# source tree or delivered in the installable package in which XiVO CTI Server
+# is distributed for more details.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+from mock import Mock
+
+from xivo_cti.tools.delta_computer import DictDelta, DeltaComputer
+
+
+class TestDeltaComputer(unittest.TestCase):
+
+    def test_compute_delta_empty_empty(self):
+        new_dict = {}
+        old_dict = {}
+        expected_result = DictDelta({}, {}, [])
+
+        result = DeltaComputer.compute_delta(new_dict, old_dict)
+
+        self.assertEqual(result, expected_result)
+
+    def test_compute_delta_add(self):
+        new_dict = {'old_key': 'old_value', 'new_key': 'new_value'}
+        old_dict = {'old_key': 'old_value'}
+        expected_result = DictDelta({'new_key': 'new_value'}, {}, [])
+
+        result = DeltaComputer.compute_delta(new_dict, old_dict)
+
+        self.assertEqual(result, expected_result)
+
+    def test_compute_delta_delete(self):
+        new_dict = {'old_key': 'old_value'}
+        old_dict = {'old_key': 'old_value', 'removed_key': 'removed_value'}
+        expected_result = DictDelta({}, {}, ['removed_key'])
+
+        result = DeltaComputer.compute_delta(new_dict, old_dict)
+
+        self.assertEqual(result, expected_result)
+
+    def test_compute_delta_change(self):
+        new_dict = {'same_key1': 'same_value', 'same_key2': 'new_value'}
+        old_dict = {'same_key1': 'same_value', 'same_key2': 'old_value'}
+        expected_result = DictDelta({}, {'same_key2': 'new_value'}, [])
+
+        result = DeltaComputer.compute_delta(new_dict, old_dict)
+
+        self.assertEqual(result, expected_result)
+
+    def test_compute_delta_change_and_add(self):
+        new_dict = {'same_key1': 'same_value', 'same_key2': 'new_value', 'new_key': 'new_value'}
+        old_dict = {'same_key1': 'same_value', 'same_key2': 'old_value'}
+        expected_result = DictDelta({'new_key': 'new_value'}, {'same_key2': 'new_value'}, [])
+
+        result = DeltaComputer.compute_delta(new_dict, old_dict)
+
+        self.assertEqual(result, expected_result)
+
+    def test_compute_delta_change_and_remove(self):
+        new_dict = {'same_key1': 'same_value', 'same_key2': 'new_value'}
+        old_dict = {'same_key1': 'same_value', 'same_key2': 'old_value', 'removed_key': 'removed_value'}
+        expected_result = DictDelta({}, {'same_key2': 'new_value'}, ['removed_key'])
+
+        result = DeltaComputer.compute_delta(new_dict, old_dict)
+
+        self.assertEqual(result, expected_result)
+
+    def test_compute_delta_add_and_remove(self):
+        new_dict = {'new_key': 'new_value'}
+        old_dict = {'removed_key': 'removed_value'}
+        expected_result = DictDelta({'new_key': 'new_value'}, {}, ['removed_key'])
+
+        result = DeltaComputer.compute_delta(new_dict, old_dict)
+
+        self.assertEqual(result, expected_result)
+
+    def test_compute_delta_add_change_and_remove(self):
+        new_dict = {'new_key': 'new_value', 'same_key': 'new_value'}
+        old_dict = {'removed_key': 'removed_value', 'same_key': 'old_value'}
+        expected_result = DictDelta({'new_key': 'new_value'}, {'same_key': 'new_value'}, ['removed_key'])
+
+        result = DeltaComputer.compute_delta(new_dict, old_dict)
+
+        self.assertEqual(result, expected_result)
