0e38a29fe7249733f334b79c89acc24906823478
Sebastien Duthil <sduthil@avencall.com>
2012-03-20 12:02:53 -0400

    [fix] missing line in setup.py

--- xivo-ctid/setup.py
+++ xivo-ctid/setup.py
@@ -20,6 +20,7 @@ setup(
               'xivo_cti.statistics',
               'xivo_cti.dao',
               'xivo_cti.dao.alchemy',
+              'xivo_cti.dao.helpers',
               'xivo_cti.cti',
               'xivo_cti.cti.commands',
               'xivo_cti.cti.commands.getlists',
