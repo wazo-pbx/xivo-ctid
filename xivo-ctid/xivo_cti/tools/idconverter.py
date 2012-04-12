7c6c2d873622547b6716e732a680dfcf296c6036
Cedric Abunar <cabunar@avencall.com>
2012-04-12 11:23:41 -0400

    [fix]normalize class

--- xivo-ctid/xivo_cti/tools/idconverter.py
+++ xivo-ctid/xivo_cti/tools/idconverter.py
@@ -22,7 +22,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-class IdConverter:
+class IdConverter(object):
     @staticmethod
     def xid_to_id(identifier):
         identifier = str(identifier)
