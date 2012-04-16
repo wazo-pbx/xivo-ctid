8c8a25aa70e6e175b8d70134e994814adbb709a5
Cedric Abunar <cabunar@avencall.com>
2012-04-16 15:33:23 -0400

    [refactor]move queue_generic to queuemember_service_manager

--- xivo-ctid/xivo_cti/cti_command.py
+++ xivo-ctid/xivo_cti/cti_command.py
@@ -826,59 +826,6 @@ class Command(object):
             return [{'amicommand': 'agentlogoff',
                      'amiargs': (agent['number'], )}]
 
-    def whenmember(self, innerdata, command, dopause, listname, k, member):
-        memberlist = []
-        midx = '%s%s:%s-%s' % (listname[0], member.get('type')[0], k, member.get('id'))
-        membership = innerdata.queuemembers.get(midx).get('membership')
-        paused = innerdata.queuemembers.get(midx).get('paused')
-        doit = False
-        if command == 'remove' and membership != 'static':
-            doit = True
-        if command == 'pause':
-            if dopause == 'true' and paused == '0':
-                doit = True
-            if dopause == 'false' and paused == '1':
-                doit = True
-        if doit:
-            memberlist.append(member)
-        return memberlist
-
-    def defmemberlist(self, innerdata, command, dopause, listname, k, member):
-        memberlist = []
-        if member.get('type') in ['phone', 'agent']:
-            membersname = member.get('type') + 'members'
-            lname = member.get('type') + 's'
-            any_members = innerdata.xod_status.get(listname).get(k).get(membersname)
-            if member.get('id') in innerdata.xod_config.get(lname).keeplist:
-                if command == 'add':
-                    if member.get('id') not in any_members:
-                        memberlist = [member]
-                else:
-                    if member.get('id') in any_members:
-                        memberlist = self.whenmember(innerdata, command, dopause, listname, k, member)
-            elif member.get('id') == 'all':
-                if command != 'add':
-                    for any_id in any_members:
-                        member = {'type': member.get('type'),
-                                  'id': any_id}
-                        memberlist = self.whenmember(innerdata, command, dopause, listname, k, member)
-        return memberlist
-
-    def makeinterfaces(self, innerdata, memberlist):
-        interfaces = []
-        for member in memberlist:
-            interface = None
-            if member.get('type') == 'phone':
-                memberstruct = innerdata.xod_config.get('phones').keeplist.get(member.get('id'))
-                interface = '%s/%s' % (memberstruct.get('protocol'), memberstruct.get('name'))
-            elif member.get('type') == 'agent':
-                memberstruct = innerdata.xod_config.get('agents').keeplist.get(member.get('id'))
-                interface = 'Agent/%s' % memberstruct.get('number')
-
-            if interface and interface not in interfaces:
-                interfaces.append(interface)
-        return interfaces
-
     def queue_generic(self, command, dopause=None):
         member = self.parseid(self._commanddict.get('member'))
         if not member:
@@ -888,47 +835,8 @@ class Command(object):
             return [{'error': 'queue'}]
 
         innerdata = self._ctiserver.safe.get(queue.get('ipbxid'))
-
-        listname = None
-        if queue.get('type') == 'queue':
-            listname = 'queues'
-        elif queue.get('type') == 'group':
-            listname = 'groups'
-
-        queuenames = []
-        interfaces = []
-
-        if listname:
-            if queue.get('id') in innerdata.xod_config.get(listname).keeplist:
-                lst = self.defmemberlist(innerdata, command, dopause, listname, queue.get('id'), member)
-                if lst:
-                    interfaces = self.makeinterfaces(innerdata, lst)
-                    queuestruct = innerdata.xod_config.get(listname).keeplist.get(queue.get('id'))
-                    queuename = queuestruct.get('name')
-                    if queuename not in queuenames:
-                        queuenames.append(queuename)
-            elif queue.get('id') == 'all':
-                for k, queuestruct in innerdata.xod_config.get(listname).keeplist.iteritems():
-                    lst = self.defmemberlist(innerdata, command, dopause, listname, k, member)
-                    if lst:
-                        interfaces = self.makeinterfaces(innerdata, lst)
-                        queuename = queuestruct.get('name')
-                        if queuename not in queuenames:
-                            queuenames.append(queuename)
-        reps = []
-        amicommand = 'queue%s' % command
-        for queuename in queuenames:
-            for interface in interfaces:
-                if command == 'remove':
-                    amiargs = (queuename, interface)
-                elif command == 'add':
-                    amiargs = (queuename, interface, dopause)
-                elif command == 'pause':
-                    amiargs = (queuename, interface, dopause)
-                rep = {'amicommand': amicommand,
-                       'amiargs': amiargs}
-                reps.append(rep)
-        return reps
+        qsm = self._ctiserver._queuemember_service_manager
+        return qsm.queue_generic(innerdata, command, queue, member, dopause)
 
     def ipbxcommand_queueadd(self):
         return self.queue_generic('add', self._commanddict.get('paused'))
