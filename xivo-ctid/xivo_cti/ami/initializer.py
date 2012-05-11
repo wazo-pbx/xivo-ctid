fe242e08a426e4684c62b94df04459472d5cd97d
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-05-11 10:18:59 -0400

    [evol] Add AMIInitializer
    Ticket #3393

---
+++ xivo-ctid/xivo_cti/ami/initializer.py
@@ -0,0 +1,117 @@
+# vim: set fileencoding=utf-8 :
+# xivo-ctid
+
+# Copyright (C) 2012 Avencall
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Alternatively, XiVO CTI Server is available under other licenses directly
+# contracted with Pro-formatique SARL. See the LICENSE file at top of the
+# source tree or delivered in the installable package in which XiVO CTI Server
+# is distributed for more details.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from xivo_cti.ami.ami_callback_handler import AMICallbackHandler
+from collections import namedtuple
+
+InitializingEntry = namedtuple('InitializingEntry', ['trigger', 'register', 'unregister', 'command'])
+
+
+class AMIInitializer(object):
+
+    FULLY_BOOTED = InitializingEntry('FullyBooted',
+                                     ['CoreShowChannelsComplete'],
+                                     [],
+                                     ['CoreShowChannels'])
+    CORE_SHOW_CHANNEL_COMPLETE = InitializingEntry('CoreShowChannelsComplete',
+                                                   ['PeerlistComplete'],
+                                                   ['CoreShowChannelsComplete'],
+                                                   ['SIPpeers', 'IAXpeers'])
+    PEER_LIST_COMPLETE = InitializingEntry('PeerlistComplete',
+                                           ['RegistrationsComplete'],
+                                           ['PeerlistComplete'],
+                                           ['SIPshowregistry', 'IAXregistry'])
+    REGISTRATION_COMPLETE = InitializingEntry('RegistrationsComplete',
+                                              ['DAHDIShowChannelsComplete'],
+                                              ['RegistrationComplete'],
+                                              ['DAHDIShowChannels'])
+    DAHDI_SHOW_CHANNELS_COMPLETE = InitializingEntry('DAHDIShowChannelsComplete',
+                                                     ['QueueSummaryComplete'],
+                                                     ['DAHDIShowChannelsComplete'],
+                                                     ['QueueSummary'])
+    QUEUE_SUMMARY_COMPLETE = InitializingEntry('QueueSummaryComplete',
+                                              ['QueueStatusComplete'],
+                                              ['QueueSummaryComplete'],
+                                              ['QueueStatus'])
+    QUEUE_STATUS_COMPLETE = InitializingEntry('QueueStatusComplete',
+                                              ['ParkedCallsComplete'],
+                                              ['QueueStatusComplete'],
+                                              ['ParkedCalls'])
+    PARKED_CALLS_COMPLETE = InitializingEntry('ParkedCallsComplete',
+                                              ['ShowStatusComplete'],
+                                              ['ParkedCallsComplete'],
+                                              ['Status'])
+    SHOW_STATUS_COMPLETE = InitializingEntry('StatusComplete',
+                                             ['ShowDialPlanComplete'],
+                                             ['StatusComplete'],
+                                             ['ShowDialPlan'])
+    SHOW_DIALPLAN_COMPLETE = InitializingEntry('ShowDialPlanComplete',
+                                               ['AgentsComplete'],
+                                               ['ShowDialPlanComplete'],
+                                               ['Agents'])
+    AGENTS_COMPLETE = InitializingEntry('AgentsComplete',
+                                        ['UserEvent'],
+                                        ['AgentsComplete'],
+                                        ['VoicemailUsersList',
+                                         'MeetmeList',
+                                         ['UserEvent', ['UserEvent', 'InitComplete']]])
+    INIT_COMPLETE = InitializingEntry('UserEvent', [], ['UserEvent'], [])
+
+    INIT_SEQUENCE = {FULLY_BOOTED.trigger: FULLY_BOOTED,
+                     CORE_SHOW_CHANNEL_COMPLETE.trigger: CORE_SHOW_CHANNEL_COMPLETE,
+                     PEER_LIST_COMPLETE.trigger: PEER_LIST_COMPLETE,
+                     REGISTRATION_COMPLETE.trigger: REGISTRATION_COMPLETE,
+                     DAHDI_SHOW_CHANNELS_COMPLETE.trigger: DAHDI_SHOW_CHANNELS_COMPLETE,
+                     QUEUE_SUMMARY_COMPLETE.trigger: QUEUE_SUMMARY_COMPLETE,
+                     QUEUE_STATUS_COMPLETE.trigger: QUEUE_STATUS_COMPLETE,
+                     PARKED_CALLS_COMPLETE.trigger: PARKED_CALLS_COMPLETE,
+                     SHOW_STATUS_COMPLETE.trigger: SHOW_STATUS_COMPLETE,
+                     SHOW_DIALPLAN_COMPLETE.trigger: SHOW_DIALPLAN_COMPLETE,
+                     INIT_COMPLETE.trigger: INIT_COMPLETE,
+                     AGENTS_COMPLETE.trigger: AGENTS_COMPLETE}
+
+    def register(self):
+        self._register('FullyBooted')
+
+    def go(self, event):
+        received = event.get('Event')
+        if received == self.INIT_COMPLETE.trigger and event['UserEvent'] != 'InitComplete':
+            return None
+        if received and received in self.INIT_SEQUENCE:
+            init = self.INIT_SEQUENCE[received]
+            map(self._register, init.register)
+            map(self._unregister, init.unregister)
+            map(self._send, init.command)
+
+    def _send(self, command):
+        if type(command) == list:
+            self._ami_class.sendcommand(*command)
+        else:
+            self._ami_class.sendcommand(command, [])
+
+    def _register(self, event):
+        self._ami_callback_handler.register_callback(event, self.go)
+
+    def _unregister(self, event):
+        self._ami_callback_handler.unregister_callback(event, self.go)
---
+++ xivo-ctid/xivo_cti/ami/tests/test_ami_initializer.py
@@ -0,0 +1,213 @@
+# vim: set fileencoding=utf-8 :
+# xivo-ctid
+
+# Copyright (C) 2012 Avencall
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Alternatively, XiVO CTI Server is available under other licenses directly
+# contracted with Pro-formatique SARL. See the LICENSE file at top of the
+# source tree or delivered in the installable package in which XiVO CTI Server
+# is distributed for more details.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import unittest
+
+from mock import Mock
+from mock import call
+
+from xivo_cti.ami.ami_callback_handler import AMICallbackHandler
+from xivo_cti.ami.initializer import AMIInitializer
+from xivo_cti.xivo_ami import AMIClass
+
+
+class TestAMIInitializer(unittest.TestCase):
+
+    def setUp(self):
+        self._ami_callback_handler = Mock(AMICallbackHandler)
+        self.ami_initializer = AMIInitializer()
+        self.ami_initializer._ami_callback_handler = self._ami_callback_handler
+        self.ami_class = Mock(AMIClass)
+        self.ami_initializer._ami_class = self.ami_class
+
+    def setup_mock(self):
+        self.ami_initializer._send = Mock()
+        self.ami_initializer._register = Mock()
+        self.ami_initializer._unregister = Mock()
+
+    def test_register(self):
+        self.setup_mock()
+        self.ami_initializer.register()
+
+        self.ami_initializer._register.assert_called_once_with('FullyBooted')
+
+    def test_go_fully_booted(self):
+        self.setup_mock()
+        msg = {'Event': 'FullyBooted',
+               'Privilege': 'system,all',
+               'Status': 'Fully Booted'}
+
+        self.ami_initializer.go(msg)
+
+        self.ami_initializer._register.assert_called_once_with('CoreShowChannelsComplete')
+        self.ami_initializer._send.assert_called_once_with('CoreShowChannels')
+
+    def test_send(self):
+        self.ami_initializer._send('test_command')
+
+        self.ami_class.sendcommand.assert_called_once_with('test_command', [])
+
+    def test_go_core_show_channels_complete(self):
+        self.setup_mock()
+        msg = {'Event': 'CoreShowChannelsComplete',
+               'EventList': 'Complete',
+               'ListItems': '0'}
+
+        self.ami_initializer.go(msg)
+
+        self.ami_initializer._register.assert_called_once_with('PeerlistComplete')
+        self.ami_initializer._unregister.assert_called_once_with('CoreShowChannelsComplete')
+        self.ami_initializer._send.assert_has_calls([call('SIPpeers'), call('IAXpeers')])
+
+    def test_go_peerlist_complete(self):
+        self.setup_mock()
+        msg = {'Event': 'PeerlistComplete',
+               'EventList': 'Complete',
+               'ListItems': '0'}
+
+
+        self.ami_initializer.go(msg)
+
+        self.ami_initializer._register.assert_called_once_with('RegistrationsComplete')
+        self.ami_initializer._unregister.assert_called_once_with('PeerlistComplete')
+        self.ami_initializer._send.assert_has_calls([call('SIPshowregistry'), call('IAXregistry')])
+
+    def test_go_registrations_complete(self):
+        self.setup_mock()
+        msg = {'Event': 'RegistrationsComplete',
+               'EventList': 'Complete',
+               'ListItems': '0'}
+
+        self.ami_initializer.go(msg)
+
+        self.ami_initializer._register.assert_called_once_with('DAHDIShowChannelsComplete')
+        self.ami_initializer._unregister.assert_called_once_with('RegistrationComplete')
+        self.ami_initializer._send.assert_called_once_with('DAHDIShowChannels')
+
+    def test_go_dahdi_show_channels_complete(self):
+        self.setup_mock()
+        msg = {'Event': 'DAHDIShowChannelsComplete',
+               'Items': '0'}
+
+        self.ami_initializer.go(msg)
+
+        self.ami_initializer._register.assert_called_once_with('QueueSummaryComplete')
+        self.ami_initializer._unregister.assert_called_once_with('DAHDIShowChannelsComplete')
+        self.ami_initializer._send.assert_called_once_with('QueueSummary')
+
+    def test_go_queue_summary_complete(self):
+        self.setup_mock()
+        msg = {'Event': 'QueueSummaryComplete'}
+
+        self.ami_initializer.go(msg)
+
+        self.ami_initializer._register.assert_called_once_with('QueueStatusComplete')
+        self.ami_initializer._unregister.assert_called_once_with('QueueSummaryComplete')
+        self.ami_initializer._send.assert_called_once_with('QueueStatus')
+
+    def test_go_queue_status_complete(self):
+        self.setup_mock()
+        msg = {'Event': 'QueueStatusComplete'}
+
+        self.ami_initializer.go(msg)
+
+        self.ami_initializer._register.assert_called_once_with('ParkedCallsComplete')
+        self.ami_initializer._unregister.assert_called_once_with('QueueStatusComplete')
+        self.ami_initializer._send('ParkedCalls')
+
+    def test_go_parked_calls_complete(self):
+        self.setup_mock()
+        msg = {'Event': 'ParkedCallsComplete',
+               'Total': '0'}
+
+        self.ami_initializer.go(msg)
+
+        self.ami_initializer._register.assert_called_once_with('ShowStatusComplete')
+        self.ami_initializer._unregister.assert_called_once_with('ParkedCallsComplete')
+        self.ami_initializer._send.assert_called_once_with('Status')
+
+    def test_go_show_status_complete(self):
+        self.setup_mock()
+        msg = {'Event': 'StatusComplete',
+               'Items': '0'}
+
+        self.ami_initializer.go(msg)
+
+        self.ami_initializer._register.assert_called_once_with('ShowDialPlanComplete')
+        self.ami_initializer._unregister.assert_called_once_with('StatusComplete')
+        self.ami_initializer._send.assert_called_once_with('ShowDialPlan')
+
+    def test_go_show_dialplan_complete(self):
+        self.setup_mock()
+        msg = {'Event': 'ShowDialPlanComplete',
+               'EventList': 'Complete',
+               'ListItems': '5619',
+               'ListExtensions': '1769',
+               'ListPriorities': '5615',
+               'ListContexts': '1769'}
+
+        self.ami_initializer.go(msg)
+
+        self.ami_initializer._register.assert_called_once_with('AgentsComplete')
+        self.ami_initializer._unregister.assert_called_once_with('ShowDialPlanComplete')
+        self.ami_initializer._send.assert_called_once_with('Agents')
+
+    def test_go_agents_complete(self):
+        self.setup_mock()
+        msg = {'Event': 'AgentsComplete'}
+
+        self.ami_initializer.go(msg)
+
+        self.ami_initializer._register.assert_called_once_with('UserEvent')
+        self.ami_initializer._unregister.assert_called_once_with('AgentsComplete')
+        self.ami_initializer._send.assert_has_calls([call('VoicemailUsersList'),
+                                                     call('MeetmeList'),
+                                                     call(['UserEvent', ['UserEvent', 'InitComplete']])])
+
+    def test_send_userevent(self):
+        self.ami_initializer._send(['UserEvent', ['UserEvent', 'My test']])
+
+        self.ami_class.sendcommand.assert_called_once_with('UserEvent', ['UserEvent', 'My test'])
+
+    def test_send_go_init_complete(self):
+        self.setup_mock()
+        msg = {'Event': 'UserEvent',
+               'Privilege': 'user,all',
+               'UserEvent': 'InitComplete',
+               'Action': 'UserEvent'}
+
+        self.ami_initializer.go(msg)
+
+        self.ami_initializer._unregister.assert_called_once_with('UserEvent')
+
+    def test_send_go_not_init_complete(self):
+        self.setup_mock()
+        msg = {'Event': 'UserEvent',
+               'UserEvent': 'Not init complete',
+               'Privilege': 'user,all',
+               'Action': 'UserEvent'}
+
+        self.ami_initializer.go(msg)
+
+        self.assertEqual(self.ami_initializer._unregister.call_count, 0)
