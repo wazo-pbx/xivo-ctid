e5ae5626f5691187a3c3204394847a2d582f4251
Cedric Abunar <cabunar@avencall.com>
2012-05-16 10:18:19 -0400

    [fix]test for extension module

--- xivo-ctid/xivo_cti/tools/tests/test_extension.py
+++ xivo-ctid/xivo_cti/tools/tests/test_extension.py
@@ -29,10 +29,17 @@ class TestExtension(unittest.TestCase):
     def test_normalize_exten(self):
         extens = ['00.11.22.33.44',
                   '00 11 22 33 44',
-                  '00-+_11@%^& 22":<>/33?:";44']
+                  '00-_11@%^& 22":<>/33?:";44']
         for exten in extens:
             self.assertEqual(normalize_exten(exten), '0011223344')
 
+    def test_normalize_exten_with_plus(self):
+        extens = ['+00.11.22.33.44',
+                  '+00 11 22 33 44',
+                  '+00-_11@%^& 22":<>/33?:";44']
+        for exten in extens:
+            self.assertEqual(normalize_exten(exten), '+0011223344')
+
     def test_normalize_exten_any_valid_char(self):
-        exten = '-+@%^& ":<>/?:";'
+        exten = '-@%^& ":<>/?:";'
         self.assertEqual(normalize_exten(exten), None)
