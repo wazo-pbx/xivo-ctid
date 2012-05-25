a53a75b039aabd4d0002d1917a4d5f66fbf5f779
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-05-25 07:28:15 -0400

    [evol] Add TrunkFeaturesDAO

---
+++ xivo-ctid/xivo_cti/dao/tests/test_trunkfeatures_dao.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8 :
+
+# Copyright (C) 2012  Avencall
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Alternatively, XiVO CTI Server is available under other licenses directly
+# contracted with Avencall. See the LICENSE file at top of the
+# source tree or delivered in the installable package in which XiVO CTI Server
+# is distributed for more details.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from xivo_cti.dao.alchemy.trunkfeatures import TrunkFeatures
+from xivo_cti.dao.alchemy.usersip import UserSIP
+from xivo_cti.dao.trunkfeaturesdao import TrunkFeaturesDAO
+from xivo_cti.dao.alchemy import dbconnection
+from xivo_cti.dao.alchemy.base import Base
+
+class TrunkFeaturesDAOTestCase(unittest.TestCase):
+
+    def setUp(self):
+        db_connection_pool = dbconnection.DBConnectionPool(dbconnection.DBConnection)
+        dbconnection.register_db_connection_pool(db_connection_pool)
+
+        uri = 'postgresql://asterisk:asterisk@localhost/asterisktest'
+        dbconnection.add_connection_as(uri, 'asterisk')
+        connection = dbconnection.get_connection('asterisk')
+
+        Base.metadata.drop_all(connection.get_engine(), [TrunkFeatures.__table__])
+        Base.metadata.create_all(connection.get_engine(), [TrunkFeatures.__table__])
+
+        Base.metadata.drop_all(connection.get_engine(), [UserSIP.__table__])
+        Base.metadata.create_all(connection.get_engine(), [UserSIP.__table__])
+
+        self.session = connection.get_session()
+
+        self.session.commit()
+        self.session = connection.get_session()
+
+        self.dao = TrunkFeaturesDAO(self.session)
+
+    def tearDown(self):
+        dbconnection.unregister_db_connection_pool()
+
+
+    def test_find_by_proto_name_sip(self):
+        trunk_name = 'my_trunk'
+
+        trunk = TrunkFeatures()
+        trunk.protocolid = 5436
+        trunk.protocol = 'sip'
+
+        usersip = UserSIP()
+        usersip.id = trunk.protocolid
+        usersip.name = trunk_name
+        usersip.type = 'peer'
+
+        self.session.add(trunk)
+        self.session.add(usersip)
+
+        self.session.commit()
+
+        result = self.dao.find_by_proto_name('sip', trunk_name)
+
+        self.assertEqual(result, trunk.id)
