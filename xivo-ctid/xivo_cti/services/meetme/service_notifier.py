1849347fd62cba4e3b9a0c60bac4e1fbee391766
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-06-05 10:17:53 -0400

    [evol] Add notification for meet me updates

--- xivo-ctid/xivo_cti/services/meetme/service_notifier.py
+++ xivo-ctid/xivo_cti/services/meetme/service_notifier.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # vim: set fileencoding=utf-8 :
 
-# Copyright (C) 2007-2011  Avencall
+# Copyright (C) 2007-2012 Avencall
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -9,9 +9,9 @@
 # (at your option) any later version.
 #
 # Alternatively, XiVO CTI Server is available under other licenses directly
-# contracted with Pro-formatique SARL. See the LICENSE file at top of the
-# source tree or delivered in the installable package in which XiVO CTI Server
-# is distributed for more details.
+# contracted with Avencall. See the LICENSE file at top of the source tree
+# or delivered in the installable package in which XiVO CTI Server is
+# distributed for more details.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -23,15 +23,23 @@
 
 import logging
 from copy import deepcopy
+from xivo_cti.services.meetme import encoder
+from xivo_cti.client_connection import ClientConnection
+from xivo_cti.cti_config import Config
 
 logger = logging.getLogger('meetme_service_notifier')
 
 
 class MeetmeServiceNotifier(object):
+
     STATUS_MESSAGE = {'class': 'getlist',
                       'listname': 'meetmes',
                       'function': '',
                       'tipbxid': ''}
+
+    def __init__(self):
+        self._subscriptions = {}
+        self._current_state = None
 
     def _prepare_message(self):
         msg = deepcopy(self.STATUS_MESSAGE)
@@ -47,3 +55,38 @@ class MeetmeServiceNotifier(object):
 
     def add(self, meetme_id):
         self.events_cti.put(self._prepare_event_add(meetme_id))
+
+    def subscribe(self, client_connection):
+        try:
+            user_id = client_connection.user_id()
+            reachable_contexts = self.user_features_dao.get_reachable_contexts(user_id)
+            channel_pattern = self.user_features_dao.get_line_identity(user_id)
+        except LookupError:
+            logger.warning('Meetme subscription failed')
+        else:
+            self._subscriptions[client_connection] = {'client_connection': client_connection,
+                                                      'contexts': reachable_contexts,
+                                                      'channel_start': channel_pattern,
+                                                      'membership': []}
+            self._push_to_client(client_connection)
+
+    def publish_meetme_update(self, meetme_status):
+        self._current_state = meetme_status
+        for client_connection in self._subscriptions:
+            self._push_to_client(client_connection)
+
+    def _send_to_client(self, client_connection, msg):
+        try:
+            client_connection.send_message(msg)
+        except ClientConnection.CloseException:
+            self._subscriptions.pop(client_connection, None)
+
+    def _push_to_client(self, client_connection):
+        if self._current_state:
+            if Config.get_instance().part_context():
+                user_id = client_connection.user_id()
+                reachable_contexts = self.user_features_dao.get_reachable_contexts(user_id)
+                msg = encoder.encode_update_for_contexts(self._current_state, reachable_contexts)
+            else:
+                msg = encoder.encode_update(self._current_state)
+            self._send_to_client(client_connection, msg)
--- xivo-ctid/xivo_cti/services/meetme/tests/test_meetme_service_notifier.py
+++ xivo-ctid/xivo_cti/services/meetme/tests/test_meetme_service_notifier.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # vim: set fileencoding=utf-8 :
 
-# Copyright (C) 2007-2011  Avencall
+# Copyright (C) 2007-2012 Avencall
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -9,9 +9,9 @@
 # (at your option) any later version.
 #
 # Alternatively, XiVO CTI Server is available under other licenses directly
-# contracted with Pro-formatique SARL. See the LICENSE file at top of the
-# source tree or delivered in the installable package in which XiVO CTI Server
-# is distributed for more details.
+# contracted with Avencall. See the LICENSE file at top of the source tree
+# or delivered in the installable package in which XiVO CTI Server is
+# distributed for more details.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -22,20 +22,31 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
+from mock import Mock, call
 from xivo_cti.services.meetme.service_notifier import MeetmeServiceNotifier
-
+from xivo_cti.services.meetme import encoder
+from xivo_cti.interfaces.interface_cti import CTI
+from xivo_cti.client_connection import ClientConnection
+from xivo_cti.dao.userfeaturesdao import UserFeaturesDAO
 import Queue
+from xivo_cti.cti_config import Config
 
 
 class TestMeetmeServiceNotifier(unittest.TestCase):
+
     def setUp(self):
         self.ipbx_id = 'xivo_test'
         self.notifier = MeetmeServiceNotifier()
         self.notifier.events_cti = Queue.Queue()
         self.notifier.ipbx_id = self.ipbx_id
+        self.config = Mock(Config)
+        self.config.part_context.return_value = False
+        Config.instance = self.config
+        self.user_dao = Mock(UserFeaturesDAO)
+        self.notifier.user_features_dao = self.user_dao
 
     def tearDown(self):
-        pass
+        Config.instance = None
 
     def test_prepare_message(self):
         result = self.notifier._prepare_message()
@@ -55,3 +66,157 @@ class TestMeetmeServiceNotifier(unittest.TestCase):
         self.assertTrue(self.notifier.events_cti.qsize() > 0)
         event = self.notifier.events_cti.get()
         self.assertEqual(event, expected)
+
+    def test_subscribe_update(self):
+        user_id = 5
+        user_contexts = ['test_ctx']
+        channel_pattern = 'SIP/abcde'
+        user_features_dao = Mock(UserFeaturesDAO)
+        user_features_dao.get_reachable_contexts.return_value = user_contexts
+        user_features_dao.get_line_identity.return_value = channel_pattern
+
+        client_connection = Mock(CTI)
+        client_connection.user_features_dao = user_features_dao
+        client_connection.user_id.return_value = user_id
+
+        self.notifier.user_features_dao = user_features_dao
+        self.notifier._current_state = {'800': {'number': '800',
+                                                 'name': 'test_conf',
+                                                 'pin_required': True,
+                                                 'start_time': 12345.123,
+                                                 'context': 'default',
+                                                 'members': {1: {'join_order': 1,
+                                                                 'join_time': 12345.123,
+                                                                 'number': '1002',
+                                                                 'name': 'Tester 1',
+                                                                 'channel': 'sip/bcde',
+                                                                 'muted': True}}}}
+
+        expected_msg = encoder.encode_update(self.notifier._current_state)
+
+        self.notifier.subscribe(client_connection)
+
+        self.assertTrue(client_connection in self.notifier._subscriptions)
+
+        expected_subscription = {'client_connection': client_connection,
+                                 'contexts': user_contexts,
+                                 'channel_start': channel_pattern,
+                                 'membership': []}
+
+        self.assertEqual(self.notifier._subscriptions[client_connection], expected_subscription)
+        client_connection.send_message.assert_called_once_with(expected_msg)
+
+    def test_publish_meetme_update(self):
+        client_connection_1 = Mock(CTI)
+        client_connection_2 = Mock(CTI)
+
+        self.notifier._subscriptions = {client_connection_1: {'client_connection': client_connection_1,
+                                                              'contexts': ['default'],
+                                                              'channel_start': 'sip/abcd',
+                                                              'membership': []},
+                                        client_connection_2: {'client_connection': client_connection_2,
+                                                              'contexts': ['default'],
+                                                              'channel_start': 'sip/bcde',
+                                                              'membership': []}}
+        msg = {'800': {'number': '800',
+                       'name': 'test_conf',
+                       'pin_required': True,
+                       'start_time': 12345.123,
+                       'context': 'default',
+                       'members': {1: {'join_order': 1,
+                                       'join_time': 12345.123,
+                                       'number': '1002',
+                                       'name': 'Tester 1',
+                                       'channel': 'sip/bcde',
+                                       'muted': True}}}}
+
+        expected_msg = encoder.encode_update(msg)
+
+        self.notifier.publish_meetme_update(msg)
+
+        client_connection_1.send_message.assert_called_once_with(expected_msg)
+        client_connection_2.send_message.assert_called_once_with(expected_msg)
+
+    def test_publish_meetme_update_context_separation(self):
+        self.config.part_context.return_value = True
+
+        client_connection_1 = Mock(CTI)
+        client_connection_2 = Mock(CTI)
+
+        self.notifier._subscriptions = {client_connection_1: {'client_connection': client_connection_1,
+                                                              'contexts': ['test'],
+                                                              'channel_start': 'sip/abcd',
+                                                              'membership': []},
+                                        client_connection_2: {'client_connection': client_connection_2,
+                                                              'contexts': ['default'],
+                                                              'channel_start': 'sip/bcde',
+                                                              'membership': []}}
+        msg = {'800': {'number': '800',
+                       'name': 'test_conf',
+                       'pin_required': True,
+                       'start_time': 12345.123,
+                       'context': 'default',
+                       'members': {1: {'join_order': 1,
+                                       'join_time': 12345.123,
+                                       'number': '1002',
+                                       'name': 'Tester 1',
+                                       'channel': 'sip/bcde',
+                                       'muted': True}}}}
+
+        _push_to_client = Mock()
+        self.notifier._push_to_client = _push_to_client
+        self.notifier.publish_meetme_update(msg)
+
+        self.assertEqual(self.notifier._current_state, msg)
+        self.assertTrue(call(client_connection_1) in _push_to_client.call_args_list)
+        self.assertTrue(call(client_connection_2) in _push_to_client.call_args_list)
+
+    def test_send_to_client(self):
+        connection = Mock(CTI)
+        msg = {'test': 'message'}
+
+        self.notifier._subscriptions = {connection: {'client_connection': connection}}
+        self.notifier._send_to_client(connection, msg)
+
+        connection.send_message.assert_called_once_with(msg)
+
+        connection.send_message.side_effect = ClientConnection.CloseException('Socket closed')
+
+        self.notifier._send_to_client(connection, msg)
+
+        self.assertTrue(connection not in self.notifier._subscriptions)
+
+    def test_push_to_client_no_context_separation(self):
+        client_connection = Mock(CTI)
+
+        msg = {'800': {'number': '800',
+                       'name': 'test_conf',
+                       'pin_required': True,
+                       'start_time': 12345.123,
+                       'context': 'default',
+                       'members': {1: {'join_order': 1,
+                                       'join_time': 12345.123,
+                                       'number': '1002',
+                                       'name': 'Tester 1',
+                                       'channel': 'sip/bcde',
+                                       'muted': True}}},
+                '801': {'number': '801',
+                       'name': 'test_conf',
+                       'pin_required': True,
+                       'start_time': 12345.123,
+                       'context': 'other',
+                       'members': {1: {'join_order': 1,
+                                       'join_time': 12345.123,
+                                       'number': '1002',
+                                       'name': 'Tester 1',
+                                       'channel': 'sip/bcde',
+                                       'muted': True}}}}
+
+        expected = encoder.encode_update(msg)
+
+        user_dao = Mock(UserFeaturesDAO)
+        self.notifier.user_features_dao = user_dao
+        self.notifier._current_state = msg
+        self.notifier._push_to_client(client_connection)
+
+        client_connection.send_message.assert_called_once_with(expected)
