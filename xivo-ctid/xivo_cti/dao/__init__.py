27d40bd94070c4933eaaeb8042454e8fa8b6fec3
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2012-12-04 10:34:57 -0500

    Add current_call.formatter

--- xivo-ctid/xivo_cti/dao/__init__.py
+++ xivo-ctid/xivo_cti/dao/__init__.py
@@ -1,15 +1,18 @@
 # XiVO CTI Server
 
 from xivo_cti.dao.agent_dao import AgentDAO
+from xivo_cti.dao.channel_dao import ChannelDAO
 from xivo_cti.dao.queue_member_dao import QueueMemberDAO
 
 agent = None
 queue_member = None
+channel = None
 
 
 def instanciate_dao(innerdata):
     global queue_member
     queue_member = QueueMemberDAO(innerdata)
-
+    global channel
+    channel = ChannelDAO(innerdata)
     global agent
     agent = AgentDAO(innerdata, queue_member)
