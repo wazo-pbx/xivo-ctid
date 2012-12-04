cbd9722b1cd03ecd300db06acded3bf18624b5bd
Cedric Abunar <cabunar@avencall.com>
2012-12-04 14:08:55 -0500

    [evo]new method to get user join lineid

--- xivo-ctid/xivo_cti/dao/userfeaturesdao.py
+++ xivo-ctid/xivo_cti/dao/userfeaturesdao.py
@@ -169,6 +169,19 @@ def all():
     return _session().query(UserFeatures).all()
 
 
+def all_join_line_id():
+    return (_session().query(UserFeatures, LineFeatures.id)
+            .outerjoin((LineFeatures, UserFeatures.id == LineFeatures.iduserfeatures))
+            .all())
+
+
+def get_join_line_id_with_user_id(user_id):
+    return (_session().query(UserFeatures, LineFeatures.id)
+            .outerjoin((LineFeatures, UserFeatures.id == LineFeatures.iduserfeatures))
+            .filter(UserFeatures.id == int(user_id))
+            .first())
+
+
 def find_by_line_id(line_id):
     return _session().query(LineFeatures.iduserfeatures).filter(LineFeatures.id == line_id)[0].iduserfeatures
 
