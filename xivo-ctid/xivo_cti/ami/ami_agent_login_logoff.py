63cf7ef4a28c9104ebd0de4632e19e16f85062d2
Etienne Lessard <elessard@avencall.com>
2012-12-04 14:07:28 -0500

    initialize agents availability from xivo-agentd instead of chan_agent
    Right now, only the logged/unlogged state works fine on initialization,
    the talking/idle status (when logged) does not work, agents are always
    initialized as idle.

--- xivo-ctid/xivo_cti/ami/ami_agent_login_logoff.py
+++ xivo-ctid/xivo_cti/ami/ami_agent_login_logoff.py
@@ -24,16 +24,11 @@
 
 import logging
 from xivo_cti.ami import ami_callback_handler
-from xivo_cti.services.agent_status import AgentStatus
 
 logger = logging.getLogger("AMIAgentLogin")
 
 
 class AMIAgentLoginLogoff(object):
-
-    AGENTSTATUS_IDLE = 'AGENT_IDLE'
-    AGENTSTATUS_ONCALL = 'AGENT_ONCALL'
-    AGENTSTATUS_WHEN_LOGGED_IN = [AGENTSTATUS_IDLE, AGENTSTATUS_ONCALL]
 
     _instance = None
 
@@ -51,34 +46,12 @@ class AMIAgentLoginLogoff(object):
         agent_id = self._build_agent_id(event['AgentNumber'])
         self.queue_statistics_producer.on_agent_loggedoff(agent_id)
 
-    def on_event_agent_init(self, event):
-        self._initialize_queue_statistics_producer(event)
-        self._initialize_agents_status(event)
-
-    def _initialize_queue_statistics_producer(self, event):
-        if (event['Status'] in self.AGENTSTATUS_WHEN_LOGGED_IN):
-            agent_id = self._build_agent_id(event['Agent'])
-            self.queue_statistics_producer.on_agent_loggedon(agent_id)
-
-    def _initialize_agents_status(self, event):
-        agent_name = event['Agent']
-        agent_id = self.agent_features_dao.agent_id(agent_name)
-        agent_status_ami = event['Status']
-        if agent_status_ami == self.AGENTSTATUS_IDLE:
-            agent_status_cti = AgentStatus.available
-        elif agent_status_ami == self.AGENTSTATUS_ONCALL:
-            agent_status_cti = AgentStatus.unavailable
-        else:
-            agent_status_cti = AgentStatus.logged_out
-        self.innerdata_dao.set_agent_availability(agent_id, agent_status_cti)
-
     @classmethod
     def register_callbacks(cls):
         callback_handler = ami_callback_handler.AMICallbackHandler.get_instance()
         ami_agent_login = cls.get_instance()
         callback_handler.register_userevent_callback('AgentLogin', ami_agent_login.on_event_agent_login)
         callback_handler.register_userevent_callback('AgentLogoff', ami_agent_login.on_event_agent_logoff)
-        callback_handler.register_callback('Agents', ami_agent_login.on_event_agent_init)
 
     @classmethod
     def get_instance(cls):
--- xivo-ctid/xivo_cti/statistics/tests/test_statistics_producer_initializer.py
+++ xivo-ctid/xivo_cti/statistics/tests/test_statistics_producer_initializer.py
@@ -1,23 +1,22 @@
+# -*- coding: utf-8 -*-
+
 import unittest
+from tests.mock import Mock
 from xivo_cti.services.queue_service_manager import QueueServiceManager
 from xivo_cti.statistics.queue_statistics_producer import QueueStatisticsProducer
-from tests.mock import Mock
 from xivo_cti.statistics.statistics_producer_initializer import StatisticsProducerInitializer
 
 
 class TestStatisticsProducerInitializer(unittest.TestCase):
 
-
     def setUp(self):
+        self.agent_client = Mock()
         self.queue_service_manager = Mock(QueueServiceManager)
         self.queue_statistics_producer = Mock(QueueStatisticsProducer)
-        self.statistics_producer_initializer = StatisticsProducerInitializer(self.queue_service_manager)
-
-
-    def tearDown(self):
-        pass
+        self.statistics_producer_initializer = StatisticsProducerInitializer(self.queue_service_manager, self.agent_client)
 
     def test_init_queue_statistics_producer_queues(self):
+        self.agent_client.get_agent_statuses.return_value = []
         self.queue_service_manager.get_queue_ids.return_value = ['12', '24']
 
         self.statistics_producer_initializer.init_queue_statistics_producer(self.queue_statistics_producer)
@@ -25,3 +24,21 @@ class TestStatisticsProducerInitializer(unittest.TestCase):
         self.queue_service_manager.get_queue_ids.assert_called_once_with()
         self.queue_statistics_producer.on_queue_added.assert_was_called_with('12')
         self.queue_statistics_producer.on_queue_added.assert_was_called_with('24')
+
+    def test_init_queue_statistics_producer_agents(self):
+        self.agent_client.get_agent_statuses.return_value = [
+            self._new_agent_status('123', True),
+            self._new_agent_status('234', False),
+        ]
+        self.queue_service_manager.get_queue_ids.return_value = []
+
+        self.statistics_producer_initializer.init_queue_statistics_producer(self.queue_statistics_producer)
+
+        self.agent_client.get_agent_statuses.assert_called_once_with()
+        self.queue_statistics_producer.on_agent_loggedon.assert_called_once_with('Agent/123')
+
+    def _new_agent_status(self, agent_number, logged):
+        agent_status = Mock()
+        agent_status.number = agent_number
+        agent_status.logged = logged
+        return agent_status
