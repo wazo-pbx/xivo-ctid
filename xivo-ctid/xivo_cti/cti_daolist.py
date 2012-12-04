55afb7968325cb75e4bac4180cdea1f3e0401205
Cedric Abunar <cabunar@avencall.com>
2012-12-04 14:14:05 -0500

    [refator]new method for getting data from dao

--- xivo-ctid/xivo_cti/cti_daolist.py
+++ xivo-ctid/xivo_cti/cti_daolist.py
@@ -26,8 +26,7 @@ import logging
 from xivo_cti.dao import userfeaturesdao
 from xivo_dao import linefeaturesdao, group_dao, agentfeaturesdao, \
     meetme_features_dao, queue_features_dao, voicemail_dao, context_dao, \
-    phonebook_dao, incall_dao
-from pprint import pprint
+    phonebook_dao, incall_dao, trunkfeaturesdao
 
 logger = logging.getLogger('daolist')
 
@@ -37,82 +36,79 @@ class DaoList(object):
     def __init__(self, listname):
         self.listname = listname
 
-    def get_list(self):
-        if self.listname == 'users':
-            return self._get_users()
-        elif self.listname == 'lines':
-            return self._get_lines()
-        elif self.listname == 'groups':
-            return self._get_groups()
-        elif self.listname == 'agents':
-            return self._get_agents()
-        elif self.listname == 'meetme':
-            return self._get_meetmes()
-        elif self.listname == 'meetme':
-            return self._get_meetmes()
-        elif self.listname == 'queues':
-            return self._get_queues()
-        elif self.listname == 'voicemail':
-            return self._get_voicemails()
-        elif self.listname == 'context':
-            return self._get_contexts()
-        elif self.listname == 'phonebook':
-            return self._get_phonebooks()
-        elif self.listname == 'incall':
-            return self._get_incalls()
+    def get(self, id):
+        try:
+            if self.listname == 'users':
+                return self._get_user(id)
+            elif self.listname == 'phones':
+                return self._get_line(id)
+            elif self.listname == 'groups':
+                return self._get_group(id)
+            elif self.listname == 'agents':
+                return self._get_agent(id)
+            elif self.listname == 'meetmes':
+                return self._get_meetme(id)
+            elif self.listname == 'queues':
+                return self._get_queue(id)
+            elif self.listname == 'voicemails':
+                return self._get_voicemail(id)
+            elif self.listname == 'contexts':
+                return self._get_context(id)
+            elif self.listname == 'phonebooks':
+                return self._get_phonebook(id)
+            elif self.listname == 'incalls':
+                return self._get_incall(id)
+            elif self.listname == 'trunks':
+                return self._get_trunk(id)
+        except LookupError:
+            return {}
 
-        return {}
+    def get_list(self):
+        try:
+            if self.listname == 'users':
+                return self._get_users()
+            elif self.listname == 'phones':
+                return self._get_lines()
+            elif self.listname == 'groups':
+                return self._get_groups()
+            elif self.listname == 'agents':
+                return self._get_agents()
+            elif self.listname == 'meetmes':
+                return self._get_meetmes()
+            elif self.listname == 'queues':
+                return self._get_queues()
+            elif self.listname == 'voicemails':
+                return self._get_voicemails()
+            elif self.listname == 'contexts':
+                return self._get_contexts()
+            elif self.listname == 'phonebooks':
+                return self._get_phonebooks()
+            elif self.listname == 'incalls':
+                return self._get_incalls()
+            elif self.listname == 'trunks':
+                return self._get_trunks()
+        except LookupError:
+            return {}
 
     def _get_users(self):
         res = {}
-        users = userfeaturesdao.all()
-        for user in users:
-            key = str(user.id)
-            res[key] = user.todict()
-            res[key]['fullname'] = '%s %s' % (user.firstname, user.lastname)
-            res[key]['identity'] = res[str(user.id)]['fullname']
+        users = userfeaturesdao.all_join_line_id()
+        for row in users:
+            user, line_id = row
+            res.update(self._format_user_data(user, line_id))
         return res
 
-    def _get_groups(self):
-        res = {}
-        groups = group_dao.all()
-        for group in groups:
-            key = str(group.id)
-            res[key] = group.todict()
-            res[key]['fullname'] = '%s (%s)' % (group.name, group.context)
-        return res
+    def _get_user(self, id):
+        user, line_id = userfeaturesdao.get_join_line_id_with_user_id(id)
+        return self._format_user_data(user, line_id)
 
-    def _get_queues(self):
+    def _format_user_data(self, user, line_id):
         res = {}
-        queues = queue_features_dao.all()
-        for queue in queues:
-            key = str(queue.id)
-            res[key] = queue.todict()
-            res[key]['identity'] = '%s (%s@%s)' % (queue.displayname, queue.number, queue.context)
-        return res
-
-    def _get_agents(self):
-        res = {}
-        agents = agentfeaturesdao.all()
-        for agent in agents:
-            res[str(agent.id)] = agent.todict()
-        return res
-
-    def _get_meetmes(self):
-        res = {}
-        meetmes = meetme_features_dao.all()
-        for meetme in meetmes:
-            res[str(meetme.id)] = meetme.todict()
-        return res
-
-    def _get_voicemails(self):
-        res = {}
-        voicemails = voicemail_dao.all()
-        for voicemail in voicemails:
-            key = str(voicemail.uniqueid)
-            res[key] = voicemail.todict()
-            res[key]['fullmailbox'] = '%s@%s' % (voicemail.mailbox, voicemail.context)
-            res[key]['identity'] = '%s (%s@%s)' % (voicemail.fullname, voicemail.mailbox, voicemail.context)
+        key = str(user.id)
+        res[key] = user.todict()
+        res[key]['fullname'] = '%s %s' % (user.firstname, user.lastname)
+        res[key]['identity'] = res[str(user.id)]['fullname']
+        res[key]['linelist'] = [str(line_id)]
         return res
 
     def _get_lines(self):
@@ -125,9 +121,107 @@ class DaoList(object):
         res = {}
         for line in full_line:
             linefeatures, protocol = line
-            merged_line = protocol.__dict__
-            merged_line.update(linefeatures.todict())
-            res[str(linefeatures.id)] = merged_line
+            res.update(self._format_line_data(linefeatures, protocol))
+        return res
+
+    def _get_line(self, id):
+        linefeatures, protocol = linefeaturesdao.get_with_line_id(id)
+        return self._format_line_data(linefeatures, protocol)
+
+    def _format_line_data(self, linefeatures, protocol):
+        res = {}
+        merged_line = protocol.__dict__
+        merged_line.update(linefeatures.todict())
+        res[str(linefeatures.id)] = merged_line
+        return res
+
+    def _get_groups(self):
+        res = {}
+        groups = group_dao.all()
+        for group in groups:
+            res.update(self._format_group_data(group))
+        return res
+
+    def _get_group(self, id):
+        group = group_dao.get(id)
+        return self._format_group_data(group)
+
+    def _format_group_data(self, group):
+        res = {}
+        key = str(group.id)
+        res[key] = group.todict()
+        res[key]['fullname'] = '%s (%s)' % (group.name, group.context)
+        return res
+
+    def _get_agents(self):
+        res = {}
+        agents = agentfeaturesdao.all()
+        for agent in agents:
+            res.update(self._format_agent_data(agent))
+        return res
+
+    def _get_agent(self, id):
+        agent = agentfeaturesdao.get(id)
+        return self._format_agent_data(agent)
+
+    def _format_agent_data(self, agent):
+        res = {}
+        key = str(agent.id)
+        res[key] = agent.todict()
+        return res
+
+    def _get_meetmes(self):
+        res = {}
+        meetmes = meetme_features_dao.all()
+        for meetme in meetmes:
+            res.update(self._format_meetme_data(meetme))
+        return res
+
+    def _get_meetme(self, id):
+        meetme = meetme_features_dao.get(id)
+        return self._format_meetme_data(meetme)
+
+    def _format_meetme_data(self, meetme):
+        res = {}
+        key = str(meetme.id)
+        res[key] = meetme.todict()
+        return res
+
+    def _get_queues(self):
+        res = {}
+        queues = queue_features_dao.all()
+        for queue in queues:
+            res.update(self._format_queue_data(queue))
+        return res
+
+    def _get_queue(self, id):
+        queue = queue_features_dao.get(id)
+        return self._format_queue_data(queue)
+
+    def _format_queue_data(self, queue):
+        res = {}
+        key = str(queue.id)
+        res[key] = queue.todict()
+        res[key]['identity'] = '%s (%s@%s)' % (queue.displayname, queue.number, queue.context)
+        return res
+
+    def _get_voicemails(self):
+        res = {}
+        voicemails = voicemail_dao.all()
+        for voicemail in voicemails:
+            res.update(self._format_voicemail_data(voicemail))
+        return res
+
+    def _get_voicemail(self, id):
+        voicemail = voicemail_dao.get(id)
+        return self._format_voicemail_data(voicemail)
+
+    def _format_voicemail_data(self, voicemail):
+        res = {}
+        key = str(voicemail.uniqueid)
+        res[key] = voicemail.todict()
+        res[key]['fullmailbox'] = '%s@%s' % (voicemail.mailbox, voicemail.context)
+        res[key]['identity'] = '%s (%s@%s)' % (voicemail.fullname, voicemail.mailbox, voicemail.context)
         return res
 
     def _get_contexts(self):
@@ -135,11 +229,20 @@ class DaoList(object):
         contexts = context_dao.all()
         for row in contexts:
             context, contextnumbers, contextinclude, contexttype = row
-            key = str(context.name)
-            res[key]['context'] = context.todict()
-            res[key]['contextnumbers'] = contextnumbers.todict()
-            res[key]['contextinclude'] = contextinclude.todict() if contextinclude else False
-            res[key]['contexttype'] = contexttype.todict()
+            res.update(self._format_context_data(self, context, contextnumbers, contextinclude, contexttype))
+        return res
+
+    def _get_context(self, id):
+        context, contextnumbers, contextinclude, contexttype = context_dao.get_join_elements(id)
+        return self._format_context_data(context, contextnumbers, contextinclude, contexttype)
+
+    def _format_context_data(self, context, contextnumbers, contextinclude, contexttype):
+        res = {}
+        key = str(context.name)
+        res[key]['context'] = context.todict()
+        res[key]['contextnumbers'] = contextnumbers.todict()
+        res[key]['contextinclude'] = contextinclude.todict() if contextinclude else False
+        res[key]['contexttype'] = contexttype.todict()
         return res
 
     def _get_phonebooks(self):
@@ -147,17 +250,86 @@ class DaoList(object):
         phonebooks = phonebook_dao.all()
         for row in phonebooks:
             phonebook, phonebookaddress, phonebooknumber = row
-            key = str(phonebook.id)
-            res[key] = {}
-            res[key]['phonebook'] = phonebook.todict()
-            res[key]['phonebookaddress'] = phonebookaddress.todict()
-            res[key]['phonebooknumber'] = phonebooknumber.todict() if phonebooknumber else False
+            res.update(self._format_phonebook_data(phonebook, phonebookaddress, phonebooknumber))
+        return res
+
+    def _get_phonebook(self, id):
+        phonebook, phonebookaddress, phonebooknumber = phonebook_dao.get_join_elements(id)
+        return self._format_phonebook_data(phonebook, phonebookaddress, phonebooknumber)
+
+    def _format_phonebook_data(self, phonebook, phonebookaddress, phonebooknumber):
+        res = {}
+        key = str(phonebook.id)
+        res[key] = {}
+        res[key]['phonebook'] = phonebook.todict()
+        res[key]['phonebookaddress'] = phonebookaddress.todict()
+        res[key]['phonebooknumber'] = phonebooknumber.todict() if phonebooknumber else False
         return res
 
     def _get_incalls(self):
         res = {}
         incalls = incall_dao.all()
         for row in incalls:
-            pprint(row)
-            incall, dialaction, user, group, queue, meetme, voicemail, voicemenu = row
+            incall, dialaction, user, group, queue, meetme, voicemail = row
+            res.update(self._format_incall_data(incall, dialaction, user, group, queue, meetme, voicemail))
+        return res
+
+    def _get_incall(self, id):
+        incall, dialaction, user, group, queue, meetme, voicemail = phonebook_dao.get_join_elements(id)
+        return self._format_incall_data(incall, dialaction, user, group, queue, meetme, voicemail)
+
+    def _format_incall_data(self, incall, dialaction, user, group, queue, meetme, voicemail):
+        res = {}
+        key = str(incall.id)
+        res[key] = {}
+        res[key]['exten'] = incall.exten
+        res[key]['context'] = incall.context
+        res[key]['preprocess_subroutine'] = incall.preprocess_subroutine
+        res[key]['commented'] = incall.commented
+        res[key]['description'] = incall.description
+
+        res[key]['linked'] = dialaction.linked
+        res[key]['action'] = dialaction.action
+        res[key]['actionarg1'] = dialaction.actionarg1
+        res[key]['actionarg2'] = dialaction.actionarg2
+
+        res[key]['userfirstname'] = user.firstname if user else None
+        res[key]['userlastname'] = user.lastname if user else None
+
+        res[key]['groupname'] = group.name if group else None
+        res[key]['groupnumber'] = group.number if group else None
+        res[key]['groupcontext'] = group.context if group else None
+
+        res[key]['queuename'] = queue.name if queue else None
+        res[key]['queuenumber'] = queue.number if queue else None
+        res[key]['queuecontext'] = queue.context if queue else None
+
+        res[key]['meetmename'] = meetme.name if meetme else None
+        res[key]['meetmenumber'] = meetme.confno if meetme else None
+        res[key]['meetmecontext'] = meetme.context if meetme else None
+
+        res[key]['voicemailfullname'] = voicemail.fullname if voicemail else None
+        res[key]['voicemailmailbox'] = voicemail.mailbox if voicemail else None
+        res[key]['voicemailcontext'] = voicemail.context if voicemail else None
+
+        res[key]['destination'] = res[key]['action']
+        res[key]['identity'] = '%s (%s)' % (res[key]['exten'], res[key]['context'])
+        res[key]['destidentity'] = '-'
+        return res
+
+    def _get_trunks(self):
+        res = {}
+        trunks = trunkfeaturesdao.all()
+        for trunk in trunks:
+            res.update(self._format_trunk_data(trunk))
+        return res
+
+    def _get_trunk(self, id):
+        trunk = trunkfeaturesdao.get(id)
+        return self._format_trunk_data(trunk)
+
+    def _format_trunk_data(self, trunk):
+        res = {}
+        key = str(trunk.id)
+        res[key] = trunk.todict()
         return res
