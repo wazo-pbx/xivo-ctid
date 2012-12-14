5bbd3f300490b3e65ac60c62c1e7c06cdde38ac2
Etienne Lessard <elessard@avencall.com>
2012-12-14 14:52:55 -0500

    add callback for AgentAddedToQueue AMI event
    Change-Id: Ia259104e598b50b34335d12d928d264bc47eba04

--- xivo-ctid/xivo_cti/services/queue_member/updater.py
+++ xivo-ctid/xivo_cti/services/queue_member/updater.py
@@ -74,6 +74,13 @@ class QueueMemberUpdater(object):
             new_state.update_as_unlogged_agent()
             self._queue_member_manager._update_queue_member(queue_member, new_state)
 
+    def on_ami_agent_added_to_queue(self, ami_event):
+        queue_name = ami_event['QueueName']
+        agent_number = ami_event['AgentNumber']
+        queue_member = QueueMember.from_ami_agent_added_to_queue(queue_name, agent_number)
+        self._queue_member_manager._add_queue_member(queue_member)
+        self._ask_member_queue_status(queue_member)
+
     def on_ami_queue_member(self, ami_event):
         queue_name = ami_event['Queue']
         member_name = ami_event['Name']
@@ -145,6 +152,7 @@ class QueueMemberUpdater(object):
 
     def register_ami_events(self, ami_handler):
         ami_handler.register_userevent_callback('AgentLogoff', self.on_ami_agent_logoff)
+        ami_handler.register_userevent_callback('AgentAddedToQueue', self.on_ami_agent_added_to_queue)
         ami_handler.register_callback('QueueMember', self.on_ami_queue_member)
         ami_handler.register_callback('QueueMemberStatus', self.on_ami_queue_member_status)
         ami_handler.register_callback('QueueMemberAdded', self.on_ami_queue_member_added)
