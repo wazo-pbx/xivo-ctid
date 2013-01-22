878a4127732d03a426cad3c8f873a4e172178d00
Gregory Eric Sanderson <gsanderson@avencall.com>
2013-01-22 16:30:41 -0500

    adjust license in python file headers
    Change-Id: Ia1b654669ab066b0b9d7f59005497a5a80ab535f

--- xivo-ctid/xivo_cti/main.py
+++ xivo-ctid/xivo_cti/main.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2012       Avencall
-# Copyright (C) 2007-2010  Proformatique
+# Copyright (C) 2007-2013 Avencall
 #
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -14,7 +13,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 import argparse
 import sys
