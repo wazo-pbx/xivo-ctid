8e8d0a1df3dc4736e1cc3b401ddae3fb09da8e85
Etienne Lessard <elessard@avencall.com>
2013-02-26 08:22:58 -0500

    optimize get_command by discriminating on command class
    This commit is mostly a quick fix to test that the optimization is worth
    it.

--- xivo-ctid/xivo_cti/cti/cti_command_factory.py
+++ xivo-ctid/xivo_cti/cti/cti_command_factory.py
@@ -15,14 +15,22 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+import collections
+
+
 class CTICommandFactory(object):
 
     _registered_classes = set()
+    _classes_by_msg_class = collections.defaultdict(list)
 
     def get_command(self, msg):
-        matching_classes = [klass for klass in self._registered_classes if klass.match_message(msg)]
+        msg_class = msg['class']
+        classes = self._classes_by_msg_class.get(msg_class,[])
+        matching_classes = [klass for klass in classes if klass.match_message(msg)]
         return matching_classes
 
     @classmethod
     def register_class(cls, klass_to_register):
+        msg_class = klass_to_register.COMMAND_CLASS
         cls._registered_classes.add(klass_to_register)
+        cls._classes_by_msg_class[msg_class].append(klass_to_register)
