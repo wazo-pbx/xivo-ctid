c73475c7cd7db5b209781e1bbe2f1823a4046a4f
Etienne Lessard <elessard@avencall.com>
2013-02-28 08:47:13 -0500

    remove unused code

--- xivo-ctid/xivo_cti/lists/agents_list.py
+++ xivo-ctid/xivo_cti/lists/agents_list.py
@@ -39,25 +39,6 @@ class AgentsList(ContextAwareAnyList):
             else:
                 logger.warning('2 agents have the same number')
 
-    def queuememberadded(self, queuename, queueorgroup, agentnumber, event):
-        qorg = '%s_by_agent' % queueorgroup
-        if agentnumber in self.reverse_index:
-            idx = self.reverse_index[agentnumber]
-            if idx in self.keeplist:
-                if queuename not in self.keeplist[idx][qorg]:
-                    self.keeplist[idx][qorg][queuename] = {}
-                    for prop in self.queuelocationprops:
-                        if prop in event:
-                            self.keeplist[idx][qorg][queuename][prop] = event.get(prop)
-
-    def queuememberremoved(self, queuename, queueorgroup, agentnumber, event):
-        qorg = '%s_by_agent' % queueorgroup
-        if agentnumber in self.reverse_index:
-            idx = self.reverse_index[agentnumber]
-            if idx in self.keeplist:
-                if queuename in self.keeplist[idx][qorg]:
-                    del self.keeplist[idx][qorg][queuename]
-
     def idbyagentnumber(self, agentnumber):
         idx = self.reverse_index.get(agentnumber)
         if idx in self.keeplist:
