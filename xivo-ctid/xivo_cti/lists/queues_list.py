95a66c71128dcd81236db49d21a24fd4b1b29bc4
Etienne Lessard <elessard@avencall.com>
2013-02-28 09:50:10 -0500

    update QueuesList reverse dictionary on add and delete

--- xivo-ctid/xivo_cti/lists/queues_list.py
+++ xivo-ctid/xivo_cti/lists/queues_list.py
@@ -28,13 +28,32 @@ class QueuesList(ContextAwareAnyList):
         ContextAwareAnyList.__init__(self, 'queues')
 
     def init_data(self):
-        ContextAwareAnyList.init_data(self)
+        super(QueuesList, self).init_data()
+        self._init_reverse_dictionary()
+
+    def add(self, queue_id):
+        super(QueuesList, self).add(queue_id)
+        self._add_to_reverse_dictionary(queue_id)
+
+    def delete(self, queue_id):
+        self._remove_from_reverse_dictionary(queue_id)
+        super(QueuesList, self).delete(queue_id)
+
+    def _init_reverse_dictionary(self):
         self.reverse_index = {}
-        for idx, ag in self.keeplist.iteritems():
-            if ag['name'] not in self.reverse_index:
-                self.reverse_index[ag['name']] = idx
-            else:
-                logger.warning('2 queues have the same name')
+        for queue_id, queue in self.keeplist.iteritems():
+            queue_name = queue['name']
+            self.reverse_index[queue_name] = queue_id
+
+    def _add_to_reverse_dictionary(self, queue_id):
+        queue = self.keeplist[queue_id]
+        queue_name = queue['name']
+        self.reverse_index[queue_name] = queue_id
+
+    def _remove_from_reverse_dictionary(self, queue_id):
+        queue = self.keeplist[queue_id]
+        queue_name = queue['name']
+        del self.reverse_index[queue_name]
 
     def hasqueue(self, queuename):
         return queuename in self.reverse_index
