16b26992879f4ebf2cdf570a98be94eda582968f
Etienne Lessard <elessard@avencall.com>
2013-02-28 10:58:11 -0500

    update AgentsList reverse dictionary on add and delete

--- xivo-ctid/xivo_cti/lists/agents_list.py
+++ xivo-ctid/xivo_cti/lists/agents_list.py
@@ -28,13 +28,32 @@ class AgentsList(ContextAwareAnyList):
         ContextAwareAnyList.__init__(self, 'agents')
 
     def init_data(self):
-        ContextAwareAnyList.init_data(self)
+        super(AgentsList, self).init_data()
+        self._init_reverse_dictionary()
+
+    def add(self, agent_id):
+        super(AgentsList, self).add(agent_id)
+        self._add_to_reverse_dictionary(agent_id)
+
+    def delete(self, agent_id):
+        self._remove_from_reverse_dictionary(agent_id)
+        super(AgentsList, self).delete(agent_id)
+
+    def _init_reverse_dictionary(self):
         self.reverse_index = {}
-        for idx, ag in self.keeplist.iteritems():
-            if ag['number'] not in self.reverse_index:
-                self.reverse_index[ag['number']] = idx
-            else:
-                logger.warning('2 agents have the same number')
+        for agent_id, agent in self.keeplist.iteritems():
+            agent_number = agent['number']
+            self.reverse_index[agent_number] = agent_id
+
+    def _add_to_reverse_dictionary(self, agent_id):
+        agent = self.keeplist[agent_id]
+        agent_number = agent['number']
+        self.reverse_index[agent_number] = agent_id
+
+    def _remove_from_reverse_dictionary(self, agent_id):
+        agent = self.keeplist[agent_id]
+        agent_number = agent['number']
+        del self.reverse_index[agent_number]
 
     def idbyagentnumber(self, agentnumber):
         idx = self.reverse_index.get(agentnumber)
