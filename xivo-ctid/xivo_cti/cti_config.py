c3c6215d4bd2f9f166fca217cea60238aa5e1a39
Cedric Abunar <cabunar@avencall.com>
2013-03-13 10:31:50 -0400

    use dao instead webservices for cti userstatus
    Change-Id: I8de7bcffd32a27df6693a40b727cfc032a45d5e1

--- xivo-ctid/xivo_cti/cti_config.py
+++ xivo-ctid/xivo_cti/cti_config.py
@@ -22,7 +22,8 @@ import ssl
 import string
 import time
 from xivo_dao import cti_service_dao, cti_preference_dao, cti_profile_dao, \
-    cti_main_dao, cti_displays_dao, cti_context_dao, cti_phonehints_dao
+    cti_main_dao, cti_displays_dao, cti_context_dao, cti_phonehints_dao, \
+    cti_userstatus_dao
 from StringIO import StringIO
 
 logger = logging.getLogger('cti_config')
@@ -77,6 +78,7 @@ class Config(object):
         self.xc_json['services'] = self._get_services()
         self.xc_json['preferences'] = self._get_preferences()
         self.xc_json['phonestatus'] = cti_phonehints_dao.get_config()
+        self.xc_json['userstatus'] = cti_userstatus_dao.get_config()
 
     def _get_profiles(self):
         profiles = cti_profile_dao.get_profiles()
