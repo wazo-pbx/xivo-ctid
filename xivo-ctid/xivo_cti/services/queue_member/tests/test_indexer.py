406abd760cd0fcc43df3ec89da04e862b0045ce6
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2013-05-08 15:42:02 -0400

    Add tests on on_queue_member_added and on_queue_member_removed
    testing when member is not an agent

--- xivo-ctid/xivo_cti/services/queue_member/tests/test_indexer.py
+++ xivo-ctid/xivo_cti/services/queue_member/tests/test_indexer.py
@@ -39,6 +39,15 @@ class TestQueueMemberIndexer(unittest.TestCase):
 
         mock_agent.add_to_queue.assert_called_once_with(13, 42)
 
+    @patch('xivo_cti.dao.agent')
+    def test_on_queue_member_added_not_agent(self, mock_agent):
+        queue_member = Mock()
+        queue_member.is_agent.return_value = False
+
+        self.queue_member_cti_indexer.on_queue_member_added(queue_member)
+
+        self.assertEquals(mock_agent.add_to_queue.call_count, 0)
+
     @patch('xivo_cti.dao.queue')
     @patch('xivo_cti.dao.agent')
     def test_on_queue_member_removed(self, mock_agent, mock_queue):
@@ -51,3 +60,12 @@ class TestQueueMemberIndexer(unittest.TestCase):
         self.queue_member_cti_indexer.on_queue_member_removed(queue_member)
 
         mock_agent.remove_from_queue.assert_called_once_with(13, 42)
+
+    @patch('xivo_cti.dao.agent')
+    def test_on_queue_member_removed_not_agent(self, mock_agent):
+        queue_member = Mock()
+        queue_member.is_agent.return_value = False
+
+        self.queue_member_cti_indexer.on_queue_member_removed(queue_member)
+
+        self.assertEquals(mock_agent.remove_from_queue.call_count, 0)
