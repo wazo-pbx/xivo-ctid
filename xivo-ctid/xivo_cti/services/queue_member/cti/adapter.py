710668655bae74ff1bc52f660f0bddef6aa14aba
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2013-10-29 15:42:26 -0400

    whitespace fix pep8 warning

--- xivo-ctid/xivo_cti/services/queue_member/cti/adapter.py
+++ xivo-ctid/xivo_cti/services/queue_member/cti/adapter.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+
 class QueueMemberCTIAdapter(object):
 
     def __init__(self, queue_member_manager):
