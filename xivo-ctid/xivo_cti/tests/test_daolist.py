89897fe815463d64cc4e29ed1776705f52daa362
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2014-01-06 12:58:28 -0500

    fix a typo in the license

--- xivo-ctid/xivo_cti/tests/test_daolist.py
+++ xivo-ctid/xivo_cti/tests/test_daolist.py
@@ -2,7 +2,7 @@
 
 # XiVO CTI Server
 
-# Copyright (C) 2007-2012  Avencall'
+# Copyright (C) 2007-2012  Avencall
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -10,7 +10,7 @@
 # (at your option) any later version.
 #
 # Alternatively, XiVO CTI Server is available under other licenses directly
-# contracted with Avencall. See the LICENSE file at top of the souce tree
+# contracted with Avencall. See the LICENSE file at top of the source tree
 # or delivered in the installable package in which XiVO CTI Server is
 # distributed for more details.
 #
