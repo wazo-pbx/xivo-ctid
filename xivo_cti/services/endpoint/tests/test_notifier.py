d3b75840a6f20d9171700d37e4ee43235dc75759
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2014-12-12 15:20:38 -0500

    add a endpoint status notifier

---
+++ xivo_cti/services/endpoint/tests/test_notifier.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import unittest
+
+from ..status_notifier import StatusNotifier
+from xivo_cti.ctiserver import CTIServer
+from mock import Mock
+
+
+class TestStatusNotifier(unittest.TestCase):
+
+    def setUp(self):
+        self._ctiserver = Mock(CTIServer)
+        self._notifier = StatusNotifier(self._ctiserver)
+
+    def test_that_notify_calls_send_cti_event(self):
+        phone_id = '42'
+        new_status = 8
+
+        self._notifier.notify(phone_id, new_status)
+
+        self._ctiserver.send_cti_event.assert_called_once_with(
+            {'class': 'getlist',
+             'listname': 'phones',
+             'function': 'updatestatus',
+             'tipbxid': 'xivo',
+             'tid': phone_id,
+             'status': {'hintstatus': new_status},
+            }
+        )
