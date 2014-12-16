887325b155da9b9dacafa721c25dceb9ccf1e53a
Etienne Lessard <elessard@avencall.com>
2014-12-16 12:09:27 -0500

    use CTIGroup in StatisticsNotifier

--- xivo_cti/statistics/statistics_notifier.py
+++ xivo_cti/statistics/statistics_notifier.py
@@ -26,27 +26,22 @@ class StatisticsNotifier(object):
     COMMAND_CLASS = 'getqueuesstats'
     CONTENT = 'stats'
 
-    def __init__(self):
-        self.cti_connections = set()
-        self.closed_cti_connections = set()
+    def __init__(self, cti_group_factory):
+        self._cti_group = cti_group_factory.new_cti_group()
 
     def on_stat_changed(self, statistic):
-        for cti_connection in self.cti_connections:
-            self.send_statistic(statistic, cti_connection)
-        self._remove_closed_connections()
+        msg = self._new_msg(statistic)
+        self._cti_group.send_message(msg)
 
     def subscribe(self, cti_connection):
-        logger.info('xivo client subscribing ')
-        self.cti_connections.add(cti_connection)
+        self._cti_group.add(cti_connection)
 
     def send_statistic(self, statistic, cti_connection):
+        msg = self._new_msg(statistic)
         try:
-            cti_connection.send_message({'class': self.COMMAND_CLASS,
-                                          self.CONTENT: statistic})
+            cti_connection.send_message(msg)
         except ClientConnection.CloseException:
-            self.closed_cti_connections.add(cti_connection)
+            pass
 
-    def _remove_closed_connections(self):
-        for closed_connection in self.closed_cti_connections:
-            self.cti_connections.remove(closed_connection)
-        self.closed_cti_connections.clear()
+    def _new_msg(self, statistic):
+        return {'class': self.COMMAND_CLASS, self.CONTENT: statistic}
