6cf69d88a994377268e38ff3823d5d3ffaf8d64d
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-01-14 09:08:48 -0500

    add the register_user_status and register_agent_status cti commands

---
+++ xivo_cti/cti/commands/register_agent_status.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2015 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import logging
+
+from xivo_cti.cti.cti_command import CTICommandClass
+
+logger = logging.getLogger(__name__)
+
+
+def _parse(msg, command):
+    logger.debug('msg: %s', msg)
+    command.agent_ids = [(xivo_uuid, agent_id) for (xivo_uuid, agent_id) in msg['agent_ids']]
+
+
+RegisterAgentStatus = CTICommandClass('register_agent_status_update', None, _parse)
+RegisterAgentStatus.add_to_registry()
