a0bc389d24da6361703f0ab2cc9a658c21bd19fa
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-03-04 13:23:11 -0500

    add an AsyncRunner to run blocking tasks
    The AsyncRunner is used to execute blocking tasks, HTTP requests for
    example and optionally call a callback function in the main loop with
    the function call result.

---
+++ xivo_cti/tests/test_async_runner.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2015 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import unittest
+
+from concurrent import futures
+from hamcrest import assert_that, equal_to
+from mock import Mock, patch, sentinel
+
+from ..async_runner import AsyncRunner
+from xivo_cti.task_queue import new_task_queue
+
+
+class TestAsyncRunner(unittest.TestCase):
+
+    def setUp(self):
+        self.thread_pool_executor = futures.ThreadPoolExecutor(max_workers=1)
+        self.task_queue = new_task_queue()
+        self.runner = AsyncRunner(self.thread_pool_executor, self.task_queue)
+
+    def tearDown(self):
+        self.task_queue.close()
+
+    def test_run_no_callback(self):
+        function = Mock()
+
+        self.runner.run(function, 'a', 42, test='lol')
+
+        self.thread_pool_executor.shutdown(wait=True)
+
+        function.assert_called_once_with('a', 42, test='lol')
+
+    def test_that_exceptions_are_logged_when_no_callback(self):
+        function = Mock(side_effect=RuntimeError)
+
+        with patch('xivo_cti.async_runner.logger') as logger:
+            self.runner.run(function)
+
+            self.thread_pool_executor.shutdown(wait=True)
+
+            function.assert_called_once_with()
+            assert_that(logger.exception.call_count, equal_to(1))
+
+    def test_run_with_call_back(self):
+        function = Mock(return_value=sentinel.result)
+        cb = Mock()
+
+        self.runner.run_with_cb(cb, function)
+
+        self.thread_pool_executor.shutdown(wait=True)
+        self.task_queue.run()
+
+        cb.assert_called_once_with(sentinel.result)
