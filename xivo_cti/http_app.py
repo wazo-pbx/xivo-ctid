6676a772dbcea7e89e5bb8ef73b0d3647997a929
Gregory Eric Sanderson <gsanderson@avencall.com>
2015-03-09 10:57:05 -0400

    inject restapi config into http app
    allows http app to check other available
    rest api options

--- xivo_cti/http_app.py
+++ xivo_cti/http_app.py
@@ -85,14 +85,14 @@ class HTTPInterface(object):
         (Users, '/users/<int:user_id>'),
     ]
 
-    def __init__(self, listen_addr, listen_port, main_thread_proxy):
+    def __init__(self, config, main_thread_proxy):
         app = Flask('xivo_ctid')
         app.wsgi_app = ProxyFix(app.wsgi_app)
         app.secret_key = os.urandom(24)
         CORS(app, allow_headers='Content-Type')
         api = restful.Api(app, prefix='/{}'.format(self.VERSION))
         self._add_resources(api, main_thread_proxy)
-        bind_addr = (listen_addr, listen_port)
+        bind_addr = (config['listen'], config['port'])
         wsgi_app = wsgiserver.WSGIPathInfoDispatcher({'/': app})
         self._server = wsgiserver.CherryPyWSGIServer(bind_addr=bind_addr,
                                                      wsgi_app=wsgi_app)
