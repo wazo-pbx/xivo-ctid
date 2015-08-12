ceba8f0a6b8d1d4bb332d5ce242e28f018b15b9a
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-08-12 14:51:29 -0400

    remove unused code in unittests

--- xivo_cti/tests/test_safe.py
+++ xivo_cti/tests/test_safe.py
@@ -40,13 +40,11 @@ class TestSafe(unittest.TestCase):
 
     _ipbx_id = 'xivo'
 
-    @patch('xivo_dao.trunk_dao.get_ids')
-    def setUp(self, mock_get_ids):
+    def setUp(self):
         queue_member_cti_adapter = Mock(QueueMemberCTIAdapter)
         self._ctiserver = Mock(CTIServer)
         self.safe = Safe(self._ctiserver, queue_member_cti_adapter)
         self.safe.user_service_manager = Mock(UserServiceManager)
-        mock_get_ids.get_ids.return_value = []
 
     def test_safe(self):
         self.assertEqual(self.safe._ctiserver, self._ctiserver)
