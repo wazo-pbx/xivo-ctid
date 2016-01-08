a976f636c059693ed8af35d8fae30c5763075174
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2016-01-08 15:21:13 -0500

    user_db: add the uuid the the user config

--- integration_tests/suite/test_user_db.py
+++ integration_tests/suite/test_user_db.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2012-2015 Avencall
+# Copyright (C) 2012-2016 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -529,6 +529,7 @@ class TestUserFeaturesDAO(test_dao.DAOTestCase):
                 'firstname': firstname,
                 'fullname': fullname,
                 'id': user_id,
+                'uuid': user_line.user.uuid,
                 'identity': fullname,
                 'incallfilter': 0,
                 'language': None,
