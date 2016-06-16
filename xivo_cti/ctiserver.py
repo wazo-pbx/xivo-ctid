eabcaa4818371e24c333a3189d792f95022c69bd
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-06-16 14:12:41 -0400

    add calls using ctid-ng

--- xivo_cti/ctiserver.py
+++ xivo_cti/ctiserver.py
@@ -303,7 +303,7 @@ class CTIServer(object):
             self._call_form_result_handler.parse, ['user_id', 'variables'],
         )
         Dial.register_callback_params(self._user_service_manager.call_destination,
-                                      ['cti_connection', 'user_id', 'destination'])
+                                      ['cti_connection', 'user_id', 'destination', 'auth_token'])
         EnableDND.register_callback_params(self._user_service_manager.enable_dnd, ['user_uuid', 'auth_token'])
         DisableDND.register_callback_params(self._user_service_manager.disable_dnd, ['user_uuid', 'auth_token'])
         EnableRecording.register_callback_params(self._user_service_manager.enable_recording, ['target'])
