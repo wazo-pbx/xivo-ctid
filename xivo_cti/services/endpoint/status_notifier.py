a429b75bde7fb6c02d9901c7c8b3bad261c44b72
Sebastien Duthil <sduthil@wazo.community>
2017-09-28 14:02:07 -0400

    bus: add user_uuid header on endpoint_status_update event

--- xivo_cti/services/endpoint/status_notifier.py
+++ xivo_cti/services/endpoint/status_notifier.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2014-2015 Avencall
+# Copyright 2014-2017 The Wazo Authors  (see the AUTHORS file)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,12 +21,16 @@ from xivo_cti.cti.cti_message_formatter import CTIMessageFormatter
 
 class StatusNotifier(object):
 
-    def __init__(self, cti_server, bus_publisher):
+    def __init__(self, cti_server, bus_publisher, innerdata):
         self._ctiserver = cti_server
         self._bus_publisher = bus_publisher
+        self._innerdata = innerdata
 
     def notify(self, phone_id, status):
         cti_event = CTIMessageFormatter.phone_hintstatus_update(phone_id, status)
         self._ctiserver.send_cti_event(cti_event)
         bus_event = EndpointStatusUpdateEvent(phone_id, status)
-        self._bus_publisher.publish(bus_event)
+        user_id = self._innerdata.xod_config['phones'].keeplist[phone_id]['iduserfeatures']
+        user_uuid = self._innerdata.xod_config['users'].keeplist[str(user_id)]['uuid']
+        headers = {'user_uuid:{uuid}'.format(uuid=user_uuid): True}
+        self._bus_publisher.publish(bus_event, headers=headers)
