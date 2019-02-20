36907d748779c4b6f1138f2b123ef65e2115321e
Francois Blackburn <fblackburn@wazo.community>
2019-02-20 09:53:33 -0500

    fix copyright

--- xivo_cti/cache_updater.py
+++ xivo_cti/cache_updater.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2006-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 import logging
